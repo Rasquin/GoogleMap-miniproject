6f930cde4a3928d46c573fa0b6565ebfcf8c4657
Rasquin <yannymk@hotmail.com>
2018-12-13 15:18:30 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,7 +1,7 @@
 //--------------------------------------------------------DISPLAY INFOR OF USERS (2ND)
 
-   // user.name is its public name
-    //user.html_url is the address to repository
+// user.name is its public name
+//user.html_url is the address to repository
 function userInformationHTML(user) {
     return `
  
@@ -20,7 +20,28 @@ function userInformationHTML(user) {
         </div>`;
 }
 
+//--------------------------------------------------------DISPLAY USER REPOSITORIES (3RD)
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
 //--------------------------------------------------------GET INFORMATION FROM GITHUB( 1ST)
 function fetchGitHubInformation(event) { // here we are writing the funtion that we call on the oninput
 
@@ -37,12 +58,17 @@ function fetchGitHubInformation(event) { // here we are writing the funtion that
 
     // Promeses to achieve---- use of the when & then
 
+    //the next when&then was modified to render two variables (username, repo)
+
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function(errorResponse) {
             if (errorResponse.status === 404) {
@@ -55,4 +81,27 @@ function fetchGitHubInformation(event) { // here we are writing the funtion that
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
+
+
+    /* Here the the function render only the user username. 
+        $.when(
+            $.getJSON(`https://api.github.com/users/${username}`)
+        ).then(
+            function(response) {
+                var userData = response;
+                $("#gh-user-data").html(userInformationHTML(userData));
+            },
+            function(errorResponse) {
+                if (errorResponse.status === 404) {
+                    $("#gh-user-data").html(
+                        `<h2>No info found for user ${username}</h2>`);
+                }
+                else {
+                    console.log(errorResponse);
+                    $("#gh-user-data").html(
+                        `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                }
+            });
+            
+          */
 }
