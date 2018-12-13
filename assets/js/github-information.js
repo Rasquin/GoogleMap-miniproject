eb1f85558580a89b1b628b376da98ce9047b09ba
Rasquin <yannymk@hotmail.com>
2018-12-13 10:24:57 +0000

    Render user information from GitHub

---
+++ assets/js/github-information.js
@@ -0,0 +1,58 @@
+//--------------------------------------------------------DISPLAY INFOR OF USERS (2ND)
+
+   // user.name is its public name
+    //user.html_url is the address to repository
+function userInformationHTML(user) {
+    return `
+ 
+        <h2>${user.name} 
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+
+//--------------------------------------------------------GET INFORMATION FROM GITHUB( 1ST)
+function fetchGitHubInformation(event) { // here we are writing the funtion that we call on the oninput
+
+    var username = $("#gh-username").val(); //remember $ means that we are using jquery
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+
+    // Promeses to achieve---- use of the when & then
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            }
+            else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
+}
